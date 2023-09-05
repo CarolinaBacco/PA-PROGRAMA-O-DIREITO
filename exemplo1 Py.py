147fe66eff9d5b6ccd7e71a6791f39468d385c1a
CarolinaBacco <141173006+CarolinaBacco@users.noreply.github.com>
2023-09-05 14:59:52 -0300

    Add files via upload

---
+++ exemplo1 Py.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+numero=10
+
+nome="carol"
+
+booleano= True
+
+a=30
+
+b=5
+
+soma = a + b
+
+multiplicacao = a * b
+
+divisao = a / b 
+
+va = True
+
+vb = False
+
+r1 = va and vb 
+r2 = va or vb 
+r3 = not va 
+
+c = 3
+
+r4 = c > 5
+r5 = c == 3
+
+
+
+
+
+
+ 
+ 
+
+
