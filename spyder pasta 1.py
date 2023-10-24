0df3b331ef4fae35cccaee8c595403c3e7d62106
CarolinaBacco <141173006+CarolinaBacco@users.noreply.github.com>
2023-10-24 14:32:32 -0300

    Add files via upload

---
+++ spyder pasta 1.py
@@ -0,0 +1,27 @@
+def soma(x,y):
+    return x+y 
+print(soma(3,5))
+print(soma(100,9))
+
+def simples(cor):
+     if cor=='azul':
+         return 'escolheu certo'
+     
+def medio(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    else:
+        return 'tente outra cor'
+    
+def completo(cor):
+       if cor == 'azul':
+            return 'escolheu certo'
+       elif cor == 'marrom':
+           return 'não tem salvação'
+       else:
+            return 'tente outra cor'
+    
+
+
+
+
